6d6a446439eb462e64d2039fbc7825a06f89a1a3
Prashwar Mishra <prashwarmishra@gmail.com>
2021-03-29 17:08:45 +0530

    add controller and route for users

---
+++ controllers/users_controller.js
@@ -0,0 +1,7 @@
+module.exports.profile = function(req, res){
+    return res.end("<h1>Codeial | Profile</h1>");
+}
+
+module.exports.posts = function(req, res){
+    return res.end("<h1>Codeial | Posts</h1>");
+}
