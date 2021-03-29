e19c8a948848aea2e5e2c36d4da68fca9025f57a
Prashwar Mishra <prashwarmishra@gmail.com>
2021-03-29 16:05:38 +0530

    Added folder directory structure

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const port = 8000;
+
+const app = express();
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in connecting to the server: ${err}`);
+        return;
+    }
+    console.log(`The server is up and running at the port: ${port}`);
+})
