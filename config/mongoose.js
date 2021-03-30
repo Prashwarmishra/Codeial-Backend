497713af5737ecf3dc80af1b109256cda6b81fed
Prashwar Mishra <prashwarmishra@gmail.com>
2021-03-30 16:55:28 +0530

    set up mongoose for database

---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/codeial_development');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to the database"));
+db.once('open', function(){
+    console.log("Connected to the server :: mongodb");
+});
--- index.js
+++ index.js
@@ -3,20 +3,25 @@ const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
+//connect to mongoose
+const db = require('./config/mongoose');
+
+//set up static files access
 app.use(express.static('./assets'));
 
+//set up express-ejs-layouts
 app.use(expressLayouts);
-
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
-
-//use express router
-app.use('/', require('./routes'));
 
 //set up view ejs
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+//use express router
+app.use('/', require('./routes'));
+
+//set up server
 app.listen(port, function(err){
     if(err){
         console.log(`Error in connecting to the server: ${err}`);
