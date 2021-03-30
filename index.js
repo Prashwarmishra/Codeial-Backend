f62a945a15742d08fd6cba47ffb1ff9f347beb69
Prashwar Mishra <prashwarmishra@gmail.com>
2021-03-30 16:36:03 +0530

    set up static files access and add css

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
+app.use(express.static('./assets'));
+
 app.use(expressLayouts);
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 //use express router
 app.use('/', require('./routes'));
