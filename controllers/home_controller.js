1b6620bf54d470405023e94d122d3d2d425b58a8
Prashwar Mishra <prashwarmishra@gmail.com>
2021-04-07 18:08:18 +0530

    populate comment's user and display the post-comments on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,6 +3,12 @@ const Post = require('../models/Post');
 module.exports.home = function(req, res){
     Post.find({})
     .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
     .exec(function(err, posts){
         if(err){
             console.log("Error in displaying user Posts");
