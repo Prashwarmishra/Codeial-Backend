0701744aa855029a715c00543a2e82d9e2403fb7
Prashwar Mishra <prashwarmishra@gmail.com>
2021-04-09 17:36:29 +0530

    add delete feature for posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/Post');
+const Comment = require('../models/Comment');
 
 module.exports.create = function(req, res){
     Post.create({
@@ -11,5 +12,26 @@ module.exports.create = function(req, res){
         }
         return res.redirect('back');
     });
+}
 
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        if(err){
+            console.log('Error in finding the post, user is trying to delete');
+            return;
+        }
+        if(post && post.user == req.user.id){
+            post.remove();
+            Comment.deleteMany({post: req.params.id}, function(err){
+                if(err){
+                    console.log('Error in deleting the comments associated with the post');
+                    return;
+                }
+                return res.redirect('back');
+            });
+        }else{
+            console.log('Cannot find the Post user is trying to delete');
+            return res.redirect('back');
+        }
+    });
 }
