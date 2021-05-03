db2d7026e97b7d2bc8fcd85ffacdf7c473700e5e
Prashwar Mishra <prashwarmishra@gmail.com>
2021-05-03 18:15:13 +0530

    create schema, controller and route for implementing like feature on Posts and Comments

---
+++ controllers/likes_controller.js
@@ -0,0 +1,54 @@
+const Like = require('../models/Like');
+const Comment = require('../models/Comment');
+const Post = require('../models/Post');
+
+module.exports.toggleLike = async function(req, res){
+    try {
+        let likeable;
+        let isDeleted = false;
+
+        //determine if likeable is a comment or a post and then fetch it from database
+        if(req.query.type == 'Post'){
+            likeable = await Post.findById(req.query.id).populate('likes');
+        }else{
+            likeable = await Comment.findById(req.query.id).populate('likes');
+        }
+
+        //check if the like exists in the database
+        let existingLike = await Like.findOne({
+            likeable: req.query.id,
+            onModel: req.query.type,
+            user: req.user._id,
+        });
+
+        //if the like exists, then remove it from the post's or comment's array and delete it
+        if(existingLike){
+            likeable.likes.pull(existingLike._id);
+            likeable.save();
+            existingLike.remove();
+            isDeleted = true;
+        }else{
+            //else create a new like 
+            let newLike = await Like.create({
+                user: req.user._id,
+                likeable: req.query.id,
+                onModel: req.query.type,
+            });
+
+            likeable.push(newLike._id);
+            likeable.save();
+        }
+
+        return res.status(200).json({
+            message: 'Like toggled',
+            data: {
+                isDeleted: isDeleted,
+            }
+        });
+    } catch (error) {
+        console.log('Error in likes Controller:', err);
+        return res.status(500).json({
+            message: 'Internal Server Error',
+        })
+    }
+}
---
+++ models/Like.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const likeSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+    likeable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel',
+    },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Comment', 'Post'],
+    }
+}, 
+    {
+        timestamps: true,
+    }
+);
+
+const Like = mongoose.model('Like', likeSchema);
+
+module.exports = Like;
