fff5b8c554f7e5d94aec5bcb3cc19abda191404c
Prashwar Mishra <prashwarmishra@gmail.com>
2021-05-13 15:59:21 +0530

    define environment variables and ready the server for production

--- config/environment.js
+++ config/environment.js
@@ -17,11 +17,28 @@ const development = {
     google_client_id: '853925351392-bcbu6jlkf3g8c2r846jrem7bm8oiff3d.apps.googleusercontent.com',
     google_client_secret: 'mqwfIEpRD6i2sWEownL_4x9V',
     google_callback_url: 'http://localhost:8000/users/auth/google/callback',
-    jwt_secret: 'codeial'
+    jwt_secret: 'codeial',
 }
 
 const production = {
     name: 'production',
+    assets_path: process.env.CODEIAL_ASSETS_PATH,
+    db_path: process.env.CODEIAL_DB_PATH,
+    session_secret: process.env.CODEIAL_SESSION_SECRET,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: 'false',
+        auth: {
+            user: process.env.CODEIAL_SMTP_USER,
+            pass: process.env.CODEIAL_SMTP_PASS,
+        }
+    },
+    google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+    google_callback_url: process.env.CODEIAL_GOOGLE_CALLBACK_URL,
+    jwt_secret: process.env.CODEIAL_JWT_SECRET,
 }
 
-module.exports = development;
+module.exports = eval(process.env.NODE_ENV) == undefined ? development : eval(process.env.NODE_ENV);
