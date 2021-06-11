885bd22051a66f5924a27f474fda54ee6c373e48
Prashwar Mishra <prashwarmishra@gmail.com>
2021-06-11 15:22:49 +0530

    disable kue

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,100 +1,105 @@
-const Post = require('../models/Post');
-const Comment = require('../models/Comment');
-const Like = require('../models/Like');
+const Post = require("../models/Post");
+const Comment = require("../models/Comment");
+const Like = require("../models/Like");
 
-const commentsMailer = require('../mailers/comments_mailer');
-const queue = require('../config/kue');
-const commentsEmailWorker = require('../workers/comments_email_worker');
+const commentsMailer = require("../mailers/comments_mailer");
+// const queue = require("../config/kue");
+// const commentsEmailWorker = require("../workers/comments_email_worker");
 
 //create a controller for adding a comment on a post
-module.exports.create = async function(req, res){
-    try{
-        //find the post on which comment has to be added
-        let post = await Post.findById(req.body.post);
+module.exports.create = async function (req, res) {
+  try {
+    //find the post on which comment has to be added
+    let post = await Post.findById(req.body.post);
 
-        //if post is found, create comment in db
-        if(post){
-            let comment = await Comment.create({
-                content: req.body.content,
-                post: req.body.post,
-                user: req.user._id,
-            });
+    //if post is found, create comment in db
+    if (post) {
+      let comment = await Comment.create({
+        content: req.body.content,
+        post: req.body.post,
+        user: req.user._id,
+      });
 
-            //push comment id to post schema's array of comments
-            post.comments.push(comment);
-            post = await post.populate('user', 'name, email').execPopulate();
-            comment = await comment.populate('user', 'name').execPopulate();
-            post.save();
-            // commentsMailer.newComment(post, comment);
-            let job = queue.create('emails', {post: post, comment: comment}).save(function(err){
-                if(err){
-                    console.log('Error in enqueuing job: ', err);
-                    return;
-                }
-                console.log('Job enqueued: ', job.id);
-            });
+      //push comment id to post schema's array of comments
+      post.comments.push(comment);
+      post = await post.populate("user", "name, email").execPopulate();
+      comment = await comment.populate("user", "name").execPopulate();
+      post.save();
+      commentsMailer.newComment({ post: post, comment: comment });
+      //   let job = queue
+      //     .create("emails", { post: post, comment: comment })
+      //     .save(function (err) {
+      //       if (err) {
+      //         console.log("Error in enqueuing job: ", err);
+      //         return;
+      //       }
+      //       console.log("Job enqueued: ", job.id);
+      //     });
 
-            if(req.xhr){
-                return res.status(200).json({
-                    data: {
-                        comment: comment,
-                        comment_user: req.user.name,
-                    }, 
-                    message: 'Comment created successfully!',
-                });
-            }
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            comment: comment,
+            comment_user: req.user.name,
+          },
+          message: "Comment created successfully!",
+        });
+      }
 
-            req.flash('success', 'Comment Added!');
-            return res.redirect('back');
-        }else{
-
-            //if post not found, redirect back
-            req.flash('error', 'Post on which the user is trying to comment is not found');
-            return res.redirect('back');
-        }
-    }catch(err){
-        req.flash('error', 'Error in adding comments on the Post');
-        return;
+      req.flash("success", "Comment Added!");
+      return res.redirect("back");
+    } else {
+      //if post not found, redirect back
+      req.flash(
+        "error",
+        "Post on which the user is trying to comment is not found"
+      );
+      return res.redirect("back");
     }
-}
+  } catch (err) {
+    req.flash("error", "Error in adding comments on the Post");
+    return;
+  }
+};
 
 //create a controller for deleting the comment
-module.exports.destroy = async function(req, res){
-    try{
-        //locate comment in database
-        let comment = await Comment.findById(req.params.id);
+module.exports.destroy = async function (req, res) {
+  try {
+    //locate comment in database
+    let comment = await Comment.findById(req.params.id);
 
-        //authenticate comment's owner
-        if(comment.user == req.user.id){
-            let postId = comment.post;
+    //authenticate comment's owner
+    if (comment.user == req.user.id) {
+      let postId = comment.post;
 
-            //delete all the likes associated with the comment
-            await Like.deleteMany({likeable: comment._id, onModel: 'Comment'});
+      //delete all the likes associated with the comment
+      await Like.deleteMany({ likeable: comment._id, onModel: "Comment" });
 
-            comment.remove();
+      comment.remove();
 
-            //delete comment's id from the post comments array in post Schema
-            await Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}});
+      //delete comment's id from the post comments array in post Schema
+      await Post.findByIdAndUpdate(postId, {
+        $pull: { comments: req.params.id },
+      });
 
-            if(req.xhr){
-                return res.status(200).json({
-                    data: {
-                        comment_id: req.params.id,
-                    },
-                    message: 'Comment deleted successfully',
-                })
-            }
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            comment_id: req.params.id,
+          },
+          message: "Comment deleted successfully",
+        });
+      }
 
-            req.flash('success', 'Comment Deleted');
-            return res.redirect('back');
-        }else{
-
-            //if comment not found, redirect back
-            req.flash('error', 'Comment not found');
-            return res.redirect('back');
-        }
-    } catch(err){
-        req.flash('error', 'Error in deleting the comment');
-        return;
+      req.flash("success", "Comment Deleted");
+      return res.redirect("back");
+    } else {
+      //if comment not found, redirect back
+      req.flash("error", "Comment not found");
+      return res.redirect("back");
     }
-}
+  } catch (err) {
+    req.flash("error", "Error in deleting the comment");
+    return;
+  }
+};
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,241 +1,237 @@
-const User = require('../models/User');
-const fs = require('fs');
-const path = require('path');
-const PasswordResetToken = require('../models/PasswordResetToken');
-const crypto = require('crypto');
-const passwordResetMailer = require('../mailers/password_reset_mailer');
-const queue = require('../config/kue');
-const resetPasswordEmailWorker = require('../workers/reset_password_email_worker');
+const User = require("../models/User");
+const fs = require("fs");
+const path = require("path");
+const PasswordResetToken = require("../models/PasswordResetToken");
+const crypto = require("crypto");
+const passwordResetMailer = require("../mailers/password_reset_mailer");
+// const queue = require("../config/kue");
+// const resetPasswordEmailWorker = require("../workers/reset_password_email_worker");
 
 //render the profile page
-module.exports.profile = async function(req, res){
-    try{
-        let user = await User.findById(req.params.id);
-        if(user){
-            return res.render('profile', {
-                title: 'Profile',
-                user_profile: user,
-            });
-        }else{
-            req.flash('error', 'user not found');
-            return res.redirect('back');
-        } 
-    }catch(err){
-        req.flash('error', 'Error in loading user profile');
-        return;
-    }  
-}
+module.exports.profile = async function (req, res) {
+  try {
+    let user = await User.findById(req.params.id);
+    if (user) {
+      return res.render("profile", {
+        title: "Profile",
+        user_profile: user,
+      });
+    } else {
+      req.flash("error", "user not found");
+      return res.redirect("back");
+    }
+  } catch (err) {
+    req.flash("error", "Error in loading user profile");
+    return;
+  }
+};
 
 //create a controller for updating user's profile
-module.exports.update = async function(req, res) {
-    try{
+module.exports.update = async function (req, res) {
+  try {
+    //authenticate user
+    if (req.user.id == req.params.id) {
+      //find User's details and update
+      let user = await User.findById(req.params.id);
 
-        //authenticate user
-        if(req.user.id == req.params.id){
-
-            //find User's details and update
-            let user = await User.findById(req.params.id);
-
-            User.uploadedAvatar(req, res, function(err){
-                if(err){
-                    console.log('****Multer error', err);
-                    return;
-                }
-                user.name = req.body.name;
-                user.email = req.body.email;
-
-                if(req.file){
-
-                    //delete if a previous avatar exists in the storage
-                    if(user.avatar){
-                        let avatarLocation = path.join(__dirname, '..', user.avatar);
-
-                        //delete only if the file is present so errors dont come up when the storage is accidently wiped out.
-                        if(fs.existsSync(avatarLocation)){
-                            fs.unlinkSync(avatarLocation);
-                        }
-                    }
-                    user.avatar = User.avatarPath + '/' + req.file.filename;
-                }
-                user.save();
-                req.flash('success', 'Profile Updated Successfully!');
-                return res.redirect('back');
-            });
-        }else{
-            return res.status(401).send('Unauthorized Access');
+      User.uploadedAvatar(req, res, function (err) {
+        if (err) {
+          console.log("****Multer error", err);
+          return;
         }
-    }catch(err){
-        req.flash('error', 'Error in updating user profile');
-        return;
+        user.name = req.body.name;
+        user.email = req.body.email;
+
+        if (req.file) {
+          //delete if a previous avatar exists in the storage
+          if (user.avatar) {
+            let avatarLocation = path.join(__dirname, "..", user.avatar);
+
+            //delete only if the file is present so errors dont come up when the storage is accidently wiped out.
+            if (fs.existsSync(avatarLocation)) {
+              fs.unlinkSync(avatarLocation);
+            }
+          }
+          user.avatar = User.avatarPath + "/" + req.file.filename;
+        }
+        user.save();
+        req.flash("success", "Profile Updated Successfully!");
+        return res.redirect("back");
+      });
+    } else {
+      return res.status(401).send("Unauthorized Access");
     }
-}
+  } catch (err) {
+    req.flash("error", "Error in updating user profile");
+    return;
+  }
+};
 
 //render the sign up page
-module.exports.signUp = function(req, res){
-    if(req.isAuthenticated()){
-        return res.redirect('/users/profile');
-    }
-    return res.render('user_sign_up', {
-        title: "Codeial | Sign Up",
-    });
-}
+module.exports.signUp = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
+  return res.render("user_sign_up", {
+    title: "Codeial | Sign Up",
+  });
+};
 
 //render the sign in page
-module.exports.signIn = function(req, res){
-    if(req.isAuthenticated()){
-        return res.redirect('/users/profile');
-    }
-    return res.render('user_sign_in', {
-        title: "Codeial | Sign In",
-    });
-}
+module.exports.signIn = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
+  return res.render("user_sign_in", {
+    title: "Codeial | Sign In",
+  });
+};
 
 //create a user database
-module.exports.create = async function(req, res){
-    try{
-        //handle password mismatch
-        if(req.body.password != req.body.confirm_password){
-            return res.redirect('back');
-        }
-
-        //find user in db
-        let user = await User.findOne({email: req.body.email});
-
-        //if user not found, create user
-        if(!user){
-            await User.create(req.body);
-            req.flash('success', 'Profile created, please sign in');
-            return res.redirect('/users/sign-in');
-        }else{
-            
-            //if user found, redirect to sign-in page
-            req.flash('error', 'Account with the email exists already');
-            return res.redirect('/users/sign-in');
-        }
-    }catch(err){
-        req.flash('error', 'Error while creating user in database');
-        return;
+module.exports.create = async function (req, res) {
+  try {
+    //handle password mismatch
+    if (req.body.password != req.body.confirm_password) {
+      return res.redirect("back");
     }
-}
 
+    //find user in db
+    let user = await User.findOne({ email: req.body.email });
+
+    //if user not found, create user
+    if (!user) {
+      await User.create(req.body);
+      req.flash("success", "Profile created, please sign in");
+      return res.redirect("/users/sign-in");
+    } else {
+      //if user found, redirect to sign-in page
+      req.flash("error", "Account with the email exists already");
+      return res.redirect("/users/sign-in");
+    }
+  } catch (err) {
+    req.flash("error", "Error while creating user in database");
+    return;
+  }
+};
 
 //create session for logging in the user
-module.exports.createSession = function(req, res){
-    req.flash('success', 'Successfully Logged In!');
-    return res.redirect('/');
-}
+module.exports.createSession = function (req, res) {
+  req.flash("success", "Successfully Logged In!");
+  return res.redirect("/");
+};
 
 //create a controller to sign out user
-module.exports.destroySession = function(req, res){
-    req.logout();
-    req.flash('success', 'Successfully Logged Out!');
-    return res.redirect('/users/sign-in');
-}
+module.exports.destroySession = function (req, res) {
+  req.logout();
+  req.flash("success", "Successfully Logged Out!");
+  return res.redirect("/users/sign-in");
+};
 
 //create controller to render forget-password page
-module.exports.forgotPassword = function(req, res){
-    if(req.isAuthenticated()){
-        return res.redirect('back');
-    }
-    return res.render('forgot_password', {
-        title: 'Forget Password',
-    })
-}
+module.exports.forgotPassword = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("back");
+  }
+  return res.render("forgot_password", {
+    title: "Forget Password",
+  });
+};
 
 //create controller to locate user by their email and send accessToken on their email
-module.exports.sendPasswordResetToken = async function(req, res){
-    try {
-        let user =  await User.findOne({email: req.body.email});
-        if(user){
+module.exports.sendPasswordResetToken = async function (req, res) {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      //if user found, create a password reset token
+      let token = await PasswordResetToken.create({
+        accessToken: crypto.randomBytes(20).toString("hex"),
+        user: user,
+        isValid: true,
+      });
 
-            //if user found, create a password reset token 
-            let token = await PasswordResetToken.create({
-                accessToken: crypto.randomBytes(20).toString('hex'),
-                user: user,
-                isValid: true,
-            });
-
-            //Enqueue the token in kue for it to be mailed
-            let job = await queue.create('reset-password-token', token).save(function(err){
-                if(err){
-                    console.log('Error in enqueueing a job: ', err);
-                    return;
-                }
-                console.log('job enqueued:', job.id);
-            })
-            //render reset information
-            return res.render('reset_password_link', {
-                title: 'Account Recovery',                    
-            })
-        }else{
-            return res.render('user_not_found', {
-                title: 'User not found',
-            });
-        }
-    } catch (error) {
-        console.log('Error in creating accessToken: ', error);
-        req.flash('error', 'Internal Server Error');
-        return res.redirect('back');
+      //Enqueue the token in kue for it to be mailed
+      passwordResetMailer.sendPasswordResetToken(token);
+      // let job = await queue.create('reset-password-token', token).save(function(err){
+      //     if(err){
+      //         console.log('Error in enqueueing a job: ', err);
+      //         return;
+      //     }
+      //     console.log('job enqueued:', job.id);
+      // })
+      //render reset information
+      return res.render("reset_password_link", {
+        title: "Account Recovery",
+      });
+    } else {
+      return res.render("user_not_found", {
+        title: "User not found",
+      });
     }
-}
+  } catch (error) {
+    console.log("Error in creating accessToken: ", error);
+    req.flash("error", "Internal Server Error");
+    return res.redirect("back");
+  }
+};
 
 //constroller to validate access token and render a reset password form
-module.exports.resetPassword = async function(req, res){
-    try {
-        //check if the token exists in the database
-        let token = await PasswordResetToken.findOne({accessToken: req.params.token});
+module.exports.resetPassword = async function (req, res) {
+  try {
+    //check if the token exists in the database
+    let token = await PasswordResetToken.findOne({
+      accessToken: req.params.token,
+    });
 
-        //if token is valid, render password reset form
-        if(token && token.isValid){
-            return res.render('reset_password', {
-                title: 'Reset Password',
-                user_token: token,
-            })
-        }else{
-
-            //else render 404
-            return res.render('fourOfour', {
-                title: '404'
-            });
-        }
-    } catch (error) {
-        console.log('Error in validating access token, ', err);
-        req.flash('error', 'Internal Server Error');
-        return res.redirect('back');
+    //if token is valid, render password reset form
+    if (token && token.isValid) {
+      return res.render("reset_password", {
+        title: "Reset Password",
+        user_token: token,
+      });
+    } else {
+      //else render 404
+      return res.render("fourOfour", {
+        title: "404",
+      });
     }
-}
+  } catch (error) {
+    console.log("Error in validating access token, ", err);
+    req.flash("error", "Internal Server Error");
+    return res.redirect("back");
+  }
+};
 
 //controller to update new password
-module.exports.updatePassword = async function(req, res){
-    try {
-        //validate the new password with the new confirm password
-        if(req.body.new_password != req.body.confirm_new_password){
-            req.flash('error', 'Password and Confirm Password did not match');
-            return res.redirect('back');
-        }
-
-        //Find user in database
-        let user = await User.findById(req.body.user_id);
-        
-        //if user found, update password
-        if(user){
-            user.password = req.body.new_password;
-            user.save();
-            let token = await PasswordResetToken.findById(req.body.token_id);
-            
-            //Also invalidate the token so the link becomes useless after one use
-            token.isValid = false;
-            token.save();
-            req.flash('success', 'Password changed Successfully!');
-            return res.redirect('/users/sign-in');
-        }else{
-            return res.render('fourOfour', {
-                title: '404',
-            })
-        }
-    } catch (error) {
-        console.log('Error in changing user password: ', error);
-        req.flash('error', 'Internal Server Error');
-        return res.redirect('back');
+module.exports.updatePassword = async function (req, res) {
+  try {
+    //validate the new password with the new confirm password
+    if (req.body.new_password != req.body.confirm_new_password) {
+      req.flash("error", "Password and Confirm Password did not match");
+      return res.redirect("back");
     }
-}
+
+    //Find user in database
+    let user = await User.findById(req.body.user_id);
+
+    //if user found, update password
+    if (user) {
+      user.password = req.body.new_password;
+      user.save();
+      let token = await PasswordResetToken.findById(req.body.token_id);
+
+      //Also invalidate the token so the link becomes useless after one use
+      token.isValid = false;
+      token.save();
+      req.flash("success", "Password changed Successfully!");
+      return res.redirect("/users/sign-in");
+    } else {
+      return res.render("fourOfour", {
+        title: "404",
+      });
+    }
+  } catch (error) {
+    console.log("Error in changing user password: ", error);
+    req.flash("error", "Internal Server Error");
+    return res.redirect("back");
+  }
+};
