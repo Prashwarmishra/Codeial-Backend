d381f89686d3e66b140777dbac4346d7f125916d
Prashwar Mishra <prashwarmishra@gmail.com>
2021-06-11 12:54:30 +0530

    add logger and rotating-file-stream

--- config/environment.js
+++ config/environment.js
@@ -1,44 +1,67 @@
+const path = require("path");
+const fs = require("fs");
+const rfs = require("rotating-file-stream");
+
+const logDirectory = path.join(__dirname, "../production_logs");
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d",
+  path: logDirectory,
+});
 
 const development = {
-    name: 'development',
-    assets_path: './assets',
-    db_path: 'codeial_development',
-    session_secret: 'BlahSomething',
-    smtp: {
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: 'false',
-        auth: {
-            user: 'prashwarmishra@gmail.com',
-            pass: '@Panzer1999',
-        }
+  name: "development",
+  assets_path: "./assets",
+  db_path: "codeial_development",
+  session_secret: "BlahSomething",
+  smtp: {
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: "false",
+    auth: {
+      user: "prashwarmishra@gmail.com",
+      pass: "@Panzer1999",
     },
-    google_client_id: '853925351392-bcbu6jlkf3g8c2r846jrem7bm8oiff3d.apps.googleusercontent.com',
-    google_client_secret: 'mqwfIEpRD6i2sWEownL_4x9V',
-    google_callback_url: 'http://localhost:8000/users/auth/google/callback',
-    jwt_secret: 'codeial',
-}
+  },
+  google_client_id:
+    "853925351392-bcbu6jlkf3g8c2r846jrem7bm8oiff3d.apps.googleusercontent.com",
+  google_client_secret: "mqwfIEpRD6i2sWEownL_4x9V",
+  google_callback_url: "http://localhost:8000/users/auth/google/callback",
+  jwt_secret: "codeial",
+  morgan: {
+    mode: "dev",
+    options: { stream: accessLogStream },
+  },
+};
 
 const production = {
-    name: 'production',
-    assets_path: process.env.CODEIAL_ASSETS_PATH,
-    db_path: process.env.CODEIAL_DB_PATH,
-    session_secret: process.env.CODEIAL_SESSION_SECRET,
-    smtp: {
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: 'false',
-        auth: {
-            user: process.env.CODEIAL_SMTP_USER,
-            pass: process.env.CODEIAL_SMTP_PASS,
-        }
+  name: "production",
+  assets_path: process.env.CODEIAL_ASSETS_PATH,
+  db_path: process.env.CODEIAL_DB_PATH,
+  session_secret: process.env.CODEIAL_SESSION_SECRET,
+  smtp: {
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: "false",
+    auth: {
+      user: process.env.CODEIAL_SMTP_USER,
+      pass: process.env.CODEIAL_SMTP_PASS,
     },
-    google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID,
-    google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
-    google_callback_url: process.env.CODEIAL_GOOGLE_CALLBACK_URL,
-    jwt_secret: process.env.CODEIAL_JWT_SECRET,
-}
+  },
+  google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID,
+  google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+  google_callback_url: process.env.CODEIAL_GOOGLE_CALLBACK_URL,
+  jwt_secret: process.env.CODEIAL_JWT_SECRET,
+  morgan: {
+    mode: "combined",
+    options: { stream: accessLogStream },
+  },
+};
 
-module.exports = eval(process.env.NODE_ENV) == undefined ? development : eval(process.env.NODE_ENV);
+module.exports =
+  eval(process.env.NODE_ENV) == undefined
+    ? development
+    : eval(process.env.NODE_ENV);
--- index.js
+++ index.js
@@ -1,73 +1,85 @@
-const express = require('express');
-const env = require('./config/environment');
+const express = require("express");
+const env = require("./config/environment");
+const logger = require("morgan");
 const port = 8000;
-const expressLayouts = require('express-ejs-layouts');
-const cookieParser = require('cookie-parser');
+const expressLayouts = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
 const app = express();
-const session = require('express-session');
-const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy');
-const passportJWT = require('./config/passport-jwt-strategy');
-const passportGoogle = require('./config/passport-google-oauth2-strategy');
-const MongoStore = require('connect-mongo');
-const sassMiddleware = require('node-sass-middleware');
-const flash = require('connect-flash');
-const customMware = require('./config/middleware');
-const path = require('path');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const passportJWT = require("./config/passport-jwt-strategy");
+const passportGoogle = require("./config/passport-google-oauth2-strategy");
+const MongoStore = require("connect-mongo");
+const sassMiddleware = require("node-sass-middleware");
+const flash = require("connect-flash");
+const customMware = require("./config/middleware");
+const path = require("path");
 
 //set up socket server
-const chatServer = require('http').Server(app);
-const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+const chatServer = require("http").Server(app);
+const chatSockets = require("./config/chat_sockets").chatSockets(chatServer);
 chatServer.listen(5000);
-console.log('Chatsockets listening on port: 5000');
+console.log("Chatsockets listening on port: 5000");
 
 //set up middlewares
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(sassMiddleware({
-    src: path.join(__dirname, env.assets_path, 'scss'),
-    dest: path.join(__dirname, env.assets_path, 'css'),
-    debug: true,
-    prefix: '/css',
-    outputStyle: 'extended',
-}));
-
+if (env.name === "development") {
+  app.use(
+    sassMiddleware({
+      src: path.join(__dirname, env.assets_path, "scss"),
+      dest: path.join(__dirname, env.assets_path, "css"),
+      debug: true,
+      prefix: "/css",
+      outputStyle: "extended",
+    })
+  );
+}
 
 //connect to mongoose
-const db = require('./config/mongoose');
-const User = require('./models/User');
+const db = require("./config/mongoose");
+const User = require("./models/User");
 
 //set up static files access
 app.use(express.static(env.assets_path));
 
 //set up access to uploaded files
-app.use('/uploads', express.static('./uploads'));
+app.use("/uploads", express.static("./uploads"));
+
+//set up logger
+app.use(logger(env.morgan.mode, env.morgan.options));
 
 //set up express-ejs-layouts
 app.use(expressLayouts);
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 //set up view ejs
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 //set up sessions
-app.use(session({
-    name: 'Codeial',
+app.use(
+  session({
+    name: "Codeial",
     secret: env.session_secret,
     saveUninitialized: false,
     resave: false,
     cookie: {
-        maxAge: (1000*60*100)
-    }, 
-    store: MongoStore.create({
-        mongoUrl: 'mongodb://localhost/codeial_development',
-        autoRemove: 'disabled',
-    }, function(err){
-        console.log(err || 'Mongo Connection Okay.');
-    }),
-}));
+      maxAge: 1000 * 60 * 100,
+    },
+    store: MongoStore.create(
+      {
+        mongoUrl: "mongodb://localhost/codeial_development",
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(err || "Mongo Connection Okay.");
+      }
+    ),
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -79,14 +91,13 @@ app.use(flash());
 app.use(customMware.setFlash);
 
 //use express router
-app.use('/', require('./routes'));
-
+app.use("/", require("./routes"));
 
 //set up server
-app.listen(port, function(err){
-    if(err){
-        console.log(`Error in connecting to the server: ${err}`);
-        return;
-    }
-    console.log(`Main server running at port: ${port}`);
-})
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in connecting to the server: ${err}`);
+    return;
+  }
+  console.log(`Main server running at port: ${port}`);
+});
